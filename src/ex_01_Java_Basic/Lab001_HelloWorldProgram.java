ea41cad1ddb1ccd0b161a7f03640bf27a32d7c79
tejas <tejasvishwanath897@gmail.com>
2025-03-11 06:59:58 +0530

    11th march

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -3,5 +3,10 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Update the Program");
+    }
+    public static void test()
+    {
+
     }
 }
