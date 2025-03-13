f6064fd8c735c77750b17cea17f96371efa06d9c
tejas <tejasvishwanath897@gmail.com>
2025-03-13 07:00:52 +0530

    13 March

---
+++ src/ex_02_Java_part2/Lab003_Assignment.java
@@ -0,0 +1,10 @@
+package ex_02_Java_part2;
+
+public class Lab003_Assignment {
+    public static void main(String[] args) {
+        int a=10,b=10,c=10;
+        int d=a*a+b*b-Math.abs(c);
+        double result =Math.cbrt(d);
+        System.out.println(result);
+    }
+}
