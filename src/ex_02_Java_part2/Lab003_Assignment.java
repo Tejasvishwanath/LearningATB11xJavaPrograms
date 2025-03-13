72557f4f806594ac51a15248e5a6f4d3e5c7623c
tejas <tejasvishwanath897@gmail.com>
2025-03-13 07:40:14 +0530

    13 March update

--- src/ex_02_Java_part2/Lab003_Assignment.java
+++ src/ex_02_Java_part2/Lab003_Assignment.java
@@ -6,5 +6,11 @@ public class Lab003_Assignment {
         int d=a*a+b*b-Math.abs(c);
         double result =Math.cbrt(d);
         System.out.println(result);
+        int X = 10, Y = 10, Z = 10 ;
+        int P=2;
+        double Result= Math.cbrt(Math.pow(X,P)+Math.pow(Y,P)-Math.abs(Z));
+        System.out.println("Result="+Result);
+
+
     }
 }
